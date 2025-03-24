7c4a5bb6c60f809f48206879d5b745b89c172e5e
Marie <lenvik.marie@gmail.com>
2025-03-24 11:01:51 +0100

    Update vite.config.js
    changed base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [vue()],
-  base: "/fed24d-grafiska-verktyg-panterkameleonterna/",
+  base: "/Grafiska-verktyg-Koduppgift/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
